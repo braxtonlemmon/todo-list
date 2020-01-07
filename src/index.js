534d92dc0e8bf6ec559af50ff4ac0ff5b70a74c5
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-01-07 18:42:42 +0100

    Add event listeners to header buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('heesy');
+const content = document.querySelector('.main');
+
+const enableHeader = () => {
+	const links = ['projects-btn', 'new-btn'];
+	links.forEach(link => {
+		const button = document.querySelector(`#${link}`);
+		button.addEventListener('click', changePage);
+	})
+}
+
+const changePage = function(e) {
+	while (content.childElementCount > 0) {
+		content.removeChild(content.lastElementChild);
+	}
+
+	switch (e.target.id) {
+		case 'projects-btn':
+			console.log('projects');
+			break;
+		case 'new-btn':
+			console.log('new');
+			break;
+	}
+}
+
+enableHeader();
